e9a61c9f88db4f9ed841e9eba7f36f4020532d44
Nitin Srivastva <159570762+NitinSrivastva@users.noreply.github.com>
2025-02-21 13:00:26 +0530

    Create Partition List.cpp

---
+++ Partition List.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* lo = new ListNode(100);
+        ListNode* hi = new ListNode(200);
+        ListNode* temp = head;
+        ListNode* templo = lo;
+        ListNode* temphi = hi;
+        while(temp!=NULL){
+            if(temp->val < x){
+                templo->next = temp;
+                temp = temp->next;
+                templo = templo->next;
+            }
+            else{
+                temphi->next = temp;
+                temp = temp->next;
+                temphi = temphi->next;
+            }
+        }
+        templo->next = hi->next;
+        temphi->next = NULL;
+        return lo->next;
+    }
+};
