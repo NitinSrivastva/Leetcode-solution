c9d50cbb80236c8cf5ce96d0b60c5574107489eb
Nitin Srivastva <159570762+NitinSrivastva@users.noreply.github.com>
2025-05-03 14:24:22 +0530

    Create Reshape The matrix.cpp

---
+++ Reshape The matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        // base condition
+        if(mat.size()*mat[0].size() != r*c){
+            return mat;
+        }
+
+        vector<vector<int>> res(r,vector<int>(c,0));
+        int x=0;
+        int y=0;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                res[x][y]=mat[i][j];
+                y++;
+                if(y==c){
+                    y=0;
+                    x++;
+                }
+            }
+        }
+        return res;
+    }
+};
